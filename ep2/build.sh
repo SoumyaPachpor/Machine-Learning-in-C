57d6c0bcdc46ded755bb7cfcc5722274fd074a1d
Your Name <your@email.com>
2026-04-10 15:04:48 +0530

    init: layer architecture

--- ep2/build.sh
+++ ep2/build.sh
@@ -3,4 +3,5 @@
 set +xe
 
 # gcc -Wall -Werror nn.c -o nn -lm
-gcc -std=c99 -Wall -Werror xor.c -o xor -lm
+# gcc -std=c99 -Wall -Werror xor.c -o xor -lm
+gcc -std=c99 -Wall -Werror layers.c -o layers -lm
